b059a8464e489b05c4a82f8518546f0e7bb96a56
AkshatWadhera <akshat7wadhera@gmail.com>
2026-06-19 11:33:03 -0400

    Handled Errors

--- server.py
+++ server.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def emotionDetector():
     text_to_analyze = request.args.get('textToAnalyze')
     result = emotion_detector(text_to_analyze)
+
+    if result['dominant_emotion'] is None:
+        return "Invalid text! Please try again!"
+
     return f"For the given statement, the system response is 'anger': {result['anger']}, 'disgust': {result['disgust']}, 'fear': {result['fear']}, 'joy': {result['joy']} and 'sadness': {result['sadness']}. The dominant emotion is {result['dominant_emotion']}."
 
 @app.route('/')
